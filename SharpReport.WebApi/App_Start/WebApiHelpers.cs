6198fc7fb5b2042ed85acf4871d2f078a8003544
iskandersierra <iskander.sierra.zaldivar@gmail.com>
2016-02-25 23:15:00 +0100

    Added DocumentsController

---
+++ SharpReport.WebApi/App_Start/WebApiHelpers.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+using System.Reflection;
+using System.Web;
+
+namespace SharpReport.WebApi
+{
+    public static class WebApiHelpers
+    {
+        public static string GetXmlCommentsPath()
+        {
+            return Path.Combine(HttpRuntime.BinDirectory, Assembly.GetExecutingAssembly().GetName().Name + ".xml");
+        }
+    }
+}
---
+++ SharpReport.WebApi/Controllers/DocumentsController.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace SharpReport.WebApi.Controllers
+{
+    /// <summary>
+    /// Manages access to user documents
+    /// </summary>
+    public class DocumentsController : ApiController
+    {
+        public async Task<IHttpActionResult> Post(CancellationToken cancellationToken)
+        {
+            // Request.Content.ReadAsMultipartAsync();
+            return BadRequest();
+        }
+    }
+}
